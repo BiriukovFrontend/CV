4bc13270051a40c0f424e6a2a12c3067854707e8
Jaro <56224207+BiriukovFrontend@users.noreply.github.com>
2022-05-08 17:59:42 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -47,3 +47,59 @@ $('.porfolio__inner').slick({
   nextArrow: '<img class="slider-arrows slider-arrows__right" src="./img/arrowR.svg" alt=""></img>',
   
 });
+$('.certification__inner').slick({
+  infinite: true,
+  speed: 300,
+  swipe:true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centreMode: true,
+  variableWidth: true,
+  prevArrow: '<img class="slider-arrows slider-arrows__left" src="./img/arrowL.svg" alt=""></img>',
+  nextArrow: '<img class="slider-arrows slider-arrows__right" src="./img/arrowR.svg" alt=""></img>',
+  
+});
+
+let linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
+  V = 0.5 // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+for (let i = 0; i < linkNav.length; i++) {
+  linkNav[i].addEventListener('click', function (e) { //по клику на ссылку
+    e.preventDefault(); //отменяем стандартное поведение
+    let w = window.pageYOffset, // производим прокрутка прокрутка
+      hash = this.href.replace(/[^#]*(.*)/, '$1'); // к id элемента, к которому нужно перейти
+    t = document.querySelector(hash).getBoundingClientRect().top, // отступ от окна браузера до id
+      start = null;
+    requestAnimationFrame(step); // подробнее про функцию анимации [developer.mozilla.org]
+    function step(time) {
+      if (start === null) start = time;
+      let progress = time - start,
+        r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+      window.scrollTo(0, r);
+      if (r != w + t) {
+        requestAnimationFrame(step)
+      } else {
+        location.hash = hash // URL с хэшем
+      }
+    }
+  }, false);
+}
+var btn = $('.arrow-top');
+var menuShow = $('.header__top');
+$(window).scroll(function() {
+if ($(window).scrollTop() > 300) {
+  btn.addClass('show');
+}
+  if ($(this).scrollTop() > 10) {
+           
+   menuShow.addClass('menuShow');
+
+} 
+else {
+  btn.removeClass('show');
+  menuShow.removeClass('menuShow');
+}
+});
+
+btn.on('click', function() {
+$('html, body').animate({scrollTop: 0 }, 1000);
+});
