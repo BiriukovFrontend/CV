5c86ca799f7cfccc5ef5c1d40b5f40fb72de361b
Jaro <56224207+BiriukovFrontend@users.noreply.github.com>
2022-06-19 17:40:40 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -31,6 +31,14 @@ $('.skills__inner').slick({
         slidesToShow: 2,
         slidesToScroll: 2
       }
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
     }
   
   ]
@@ -57,32 +65,33 @@ $('.certification__inner').slick({
   variableWidth: true,
   prevArrow: '<img class="slider-arrows slider-arrows__left" src="./img/arrowL.svg" alt=""></img>',
   nextArrow: '<img class="slider-arrows slider-arrows__right" src="./img/arrowR.svg" alt=""></img>',
-  
-});
-
-let linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
-  V = 0.5 // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
-for (let i = 0; i < linkNav.length; i++) {
-  linkNav[i].addEventListener('click', function (e) { //по клику на ссылку
-    e.preventDefault(); //отменяем стандартное поведение
-    let w = window.pageYOffset, // производим прокрутка прокрутка
-      hash = this.href.replace(/[^#]*(.*)/, '$1'); // к id элемента, к которому нужно перейти
-    t = document.querySelector(hash).getBoundingClientRect().top-70, // отступ от окна браузера до id
-      start = null;
-    requestAnimationFrame(step); // подробнее про функцию анимации [developer.mozilla.org]
-    function step(time) {
-      if (start === null) start = time;
-      let progress = time - start,
-        r = (t <0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
-      window.scrollTo(0, r);
-      if (r != w + t) {
-        requestAnimationFrame(step)
-      } else {
-        location.hash = hash // URL с хэшем
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+       
       }
     }
-  }, false);
-}
+    ]
+});
+
+
+
+$(function(){
+    $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top - ($("header").outerHeight() - 350); 
+                $('html,body').animate({scrollTop: targetOffset}, 600);
+                return false;
+            }
+        }
+    });
+});
 var btn = $('.arrow-top');
  var menuShow = $('.header__top');
  let headerNav = $('.header__nav');
@@ -94,10 +103,8 @@ $(window).scroll(function() {
 if ($(window).scrollTop() > 300) {
   btn.addClass('show');
 }
-if ($(this).width() > 1000){
-  navMenu.css('display', 'none');
-}
-  if ($(this).scrollTop() > 10) {
+
+ if ($(this).scrollTop() > 10) {
    menuShow.addClass('menuFixed'); 
   navMenu.click(function(){
     headerNav.css({
@@ -123,16 +130,14 @@ navMenuClose.click(function(){
   $('html').css('overflow', 'auto');
 });
   });
-
-
-
   } 
-
-
-
+/*   if ($(this).width() >= 1000){
+    navMenu.css('display', 'none');
+  } */
 else {
-  btn.removeClass('show');
+btn.removeClass('show');
  menuShow.removeClass('menuFixed');
+
 
 }
 
